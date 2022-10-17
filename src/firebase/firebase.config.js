02bb82da25244a2662c731050fd61f9a22620494
mursona <mursona>
2022-10-17 13:54:11 +0600

    firebase config

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDxCGxPQ41CRB-_B5LlH9JTXn2IIYJb1RY",
+  authDomain: "ema-john-react-router-au-97b52.firebaseapp.com",
+  projectId: "ema-john-react-router-au-97b52",
+  storageBucket: "ema-john-react-router-au-97b52.appspot.com",
+  messagingSenderId: "246685167163",
+  appId: "1:246685167163:web:21d134c88f2194b574f61e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
